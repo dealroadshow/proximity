899215d988c9958bab64bea70a28b2c82a4bdd25
petr-buchyn <petr.buchyn@finsight.com>
2023-03-20 04:33:03 +0200

    Default interceptors

--- src/ProxyOptions.php
+++ src/ProxyOptions.php
@@ -10,18 +10,24 @@ readonly class ProxyOptions
     /**
      * @param BodyInterceptorInterface[][] $bodyInterceptors
      * @param ResultInterceptorInterface[][] $resultInterceptors
+     * @param BodyInterceptorInterface[] $defaultBodyInterceptors
+     * @param ResultInterceptorInterface[] $defaultResultInterceptors
      */
-    public function __construct(public array $bodyInterceptors, public array $resultInterceptors)
-    {
+    public function __construct(
+        public array $bodyInterceptors = [],
+        public array $resultInterceptors = [],
+        public array $defaultBodyInterceptors = [],
+        public array $defaultResultInterceptors = []
+    ) {
     }
 
     public function bodyInterceptorsForMethod(string $methodName): array
     {
-        return $this->bodyInterceptors[$methodName] ?? [];
+        return $this->bodyInterceptors[$methodName] ?? $this->defaultBodyInterceptors;
     }
 
     public function resultInterceptorsForMethod(string $methodName): array
     {
-        return $this->resultInterceptors[$methodName] ?? [];
+        return $this->resultInterceptors[$methodName] ?? $this->defaultResultInterceptors;
     }
 }
